66df19cc1a745319c3898d71416ead6ce3510da5
Dep121 <deepakkumarpandey121@gmail.com>
2017-01-27 23:12:30 +0530

    8.1-syncing task

--- app/src/main/java/dep/todoekspert/LoginActivity.java
+++ app/src/main/java/dep/todoekspert/LoginActivity.java
@@ -1,6 +1,7 @@
 package dep.todoekspert;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.text.TextUtilsCompat;
 import android.support.v4.util.PatternsCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -52,11 +53,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void login(String username, String password) {
-        if (username.equals("test") && password.equals("test")) {
-            Toast.makeText(getApplicationContext(), R.string.login_ok, Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getApplicationContext(), TodoListActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        AsyncTask<String, Integer, Boolean> asyncTask = new AsyncTask<String, Integer, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                String username = params[0];
+                String password = params[1];
+                try{
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return username.equals("test") && password.equals("test");
+            }
+
+            @Override
+            protected void onPostExecute(Boolean logged) {
+                super.onPostExecute(logged);
+                button.setEnabled(true);
+                if (logged){
+                    Toast.makeText(getApplicationContext(), R.string.login_ok, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), TodoListActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                button.setEnabled(false);
+            }
+        };
+
+        asyncTask.execute(username, password);
     }
 }
