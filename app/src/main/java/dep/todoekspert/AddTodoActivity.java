d3bf5e7517b1292a67947150b26c525d1f728e67
Dep121 <deepakkumarpandey121@gmail.com>
2017-01-27 14:58:45 +0530

    6-Add todo activity layout and some code

--- app/src/main/java/dep/todoekspert/AddTodoActivity.java
+++ app/src/main/java/dep/todoekspert/AddTodoActivity.java
@@ -2,12 +2,34 @@ package dep.todoekspert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class AddTodoActivity extends AppCompatActivity {
+
+    private EditText contentEditText;
+    private CheckBox doneCheckBox;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
+        contentEditText = (EditText) findViewById(R.id.content_et);
+        doneCheckBox = (CheckBox) findViewById(R.id.done_cb);
+        saveButton = (Button) findViewById(R.id.save_bt);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = contentEditText.getText().toString();
+                boolean isdone = doneCheckBox.isChecked();
+
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/dep/todoekspert/TodoListActivity.java
+++ app/src/main/java/dep/todoekspert/TodoListActivity.java
@@ -6,14 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.R.id.content;
+import static android.R.id.edit;
 
 public class TodoListActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 123;
     ListActivity la;
+    private CheckBox doneCheckBox;
+    private EditText contentEditText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
     }
 
     @Override
@@ -35,9 +48,22 @@ public class TodoListActivity extends AppCompatActivity {
         else if (id == R.id.action_add)
         {
             Intent intent = new Intent(getApplicationContext(), AddTodoActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_CODE);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(getApplication(), "Result OK", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode == RESULT_CANCELED){
+                Toast.makeText(getApplicationContext(), "Result Cancled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
